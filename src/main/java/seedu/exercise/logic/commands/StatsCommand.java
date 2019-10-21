294c0dcc7d79e7022466cadf2d8b9defa21c2b2f
jietung <jietungchai@gmail.com>
2019-10-21 20:34:41 +0800

    Add StatsCommand

---
+++ src/main/java/seedu/exercise/logic/commands/StatsCommand.java
@@ -0,0 +1,57 @@
+package seedu.exercise.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.exercise.logic.parser.CliSyntax.PREFIX_CATEGORY;
+import static seedu.exercise.logic.parser.CliSyntax.PREFIX_CHART;
+import static seedu.exercise.logic.parser.CliSyntax.PREFIX_ENDDATE;
+import static seedu.exercise.logic.parser.CliSyntax.PREFIX_STARTDATE;
+
+import javafx.scene.chart.Chart;
+import seedu.exercise.logic.commands.exceptions.CommandException;
+import seedu.exercise.model.Model;
+import seedu.exercise.model.ReadOnlyResourceBook;
+import seedu.exercise.model.property.Date;
+import seedu.exercise.model.resource.Exercise;
+
+public class StatsCommand extends Command {
+
+    public static final String COMMAND_WORD = "stats";
+
+    public static final String MESSAGE_USAGE = "Parameters: "
+        + PREFIX_CATEGORY + "CATEGORY "
+        + PREFIX_CHART + "CHART TYPE "
+        + PREFIX_STARTDATE + "START DATE "
+        + PREFIX_ENDDATE + "END DATE " + "\n"
+        + "Example: " + COMMAND_WORD + " "
+        + PREFIX_CATEGORY + "calories "
+        + PREFIX_CHART + "barchart "
+        + PREFIX_STARTDATE + "30/03/2019 "
+        + PREFIX_ENDDATE + "05/04/2019 ";
+
+    private String chart;
+    private String category;
+    private Date startDate;
+    private Date endDate;
+
+    public StatsCommand(String chart, String category, Date startDate, Date endDate) {
+        this.chart = chart;
+        this.category = category;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        ReadOnlyResourceBook<Exercise> exercises = model.getExerciseBookData();
+        StatsFactory statsFactory = new StatsFactory(exercises, chart, category, startDate, endDate);
+        Chart chart = statsFactory.generateChart();
+
+        return new CommandResult("Chart displayed");
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
+    }
+}
