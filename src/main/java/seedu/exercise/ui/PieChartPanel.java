7dbc682c94e188de1486f6a2b38035d658bc4906
jietung <jietungchai@gmail.com>
2019-10-22 17:14:33 +0800

    Add PieChartPanel

---
+++ src/main/java/seedu/exercise/ui/PieChartPanel.java
@@ -0,0 +1,36 @@
+package seedu.exercise.ui;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.Region;
+import seedu.exercise.logic.commands.statistic.Statistic;
+
+public class PieChartPanel extends UiPart<Region> {
+    private static final String FXML = "PieChartPanel.fxml";
+
+    private Statistic statistic;
+
+    @FXML
+    private PieChart pieChart;
+
+    public PieChartPanel(Statistic statistic) {
+        super(FXML);
+        this.statistic = statistic;
+        display();
+    }
+
+    private void display() {
+        ArrayList<String> names = statistic.getProperties();
+        ArrayList<Double> values = statistic.getValues();
+
+        int size = names.size();
+        for (int i = 0; i < size; i++) {
+            PieChart.Data slice = new PieChart.Data(names.get(i), values.get(i));
+            pieChart.getData().add(slice);
+        }
+
+        pieChart.setTitle(statistic.getCategory());
+    }
+}
