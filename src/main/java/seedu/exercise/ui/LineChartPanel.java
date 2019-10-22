24d8594f5b9dbe8cd19fffb3f9d8c13bcd0e804c
jietung <jietungchai@gmail.com>
2019-10-22 17:14:21 +0800

    Add LineChartPanel

---
+++ src/main/java/seedu/exercise/ui/LineChartPanel.java
@@ -0,0 +1,52 @@
+package seedu.exercise.ui;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.Region;
+import seedu.exercise.logic.commands.statistic.Statistic;
+
+public class LineChartPanel extends UiPart<Region> {
+
+    private static final String FXML = "LineChartPanel.fxml";
+
+    private Statistic statistic;
+
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private LineChart<String, Double> lineChart;
+
+    public LineChartPanel(Statistic statistic) {
+        super(FXML);
+        this.statistic = statistic;
+        display();
+    }
+
+    private void display() {
+        lineChart.setAnimated(false);
+        lineChart.layout();
+
+        xAxis.setLabel("date");
+        yAxis.setLabel(statistic.getCategory());
+
+        XYChart.Series series = new XYChart.Series();
+
+        ArrayList<String> dates = statistic.getProperties();
+        ArrayList<Double> values = statistic.getValues();
+
+        int size = dates.size();
+        for (int i = 0; i < size; i++) {
+            series.getData().add(new XYChart.Data<>(dates.get(i), values.get(i)));
+        }
+
+        lineChart.setLegendVisible(false);
+        lineChart.getData().add(series);
+    }
+}
