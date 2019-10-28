a2ce6f9faef068ab6aeef12c4f090e9577575b8f
jietung <jietungchai@gmail.com>
2019-10-28 18:47:27 +0800

    Move static final Strings to CommonTestData

--- src/test/java/seedu/exercise/logic/commands/StatsFactoryTest.java
+++ src/test/java/seedu/exercise/logic/commands/StatsFactoryTest.java
@@ -1,6 +1,11 @@
 package seedu.exercise.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.exercise.testutil.CommonTestData.VALID_BAR_CHART;
+import static seedu.exercise.testutil.CommonTestData.VALID_LINE_CHART;
+import static seedu.exercise.testutil.CommonTestData.VALID_PIE_CHART;
+import static seedu.exercise.testutil.CommonTestData.VALID_STATISTIC_CATEGORY_CALORIES;
+import static seedu.exercise.testutil.CommonTestData.VALID_STATISTIC_CATEGORY_EXERCISE;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,12 +18,14 @@ import seedu.exercise.logic.commands.statistic.StatsFactory;
 import seedu.exercise.model.ReadOnlyResourceBook;
 import seedu.exercise.model.property.Date;
 import seedu.exercise.model.resource.Exercise;
-import seedu.exercise.testutil.ExerciseBookBuilder;
-import seedu.exercise.testutil.StatisticBuilder;
-import seedu.exercise.testutil.exercise.ExerciseBuilder;
+import seedu.exercise.testutil.builder.ExerciseBookBuilder;
+import seedu.exercise.testutil.builder.ExerciseBuilder;
+import seedu.exercise.testutil.builder.StatisticBuilder;
 
 public class StatsFactoryTest {
 
+    private static final String START_DATE = "25/09/2019";
+    private static final String END_DATE = "30/09/2019";
     private ReadOnlyResourceBook<Exercise> eb;
 
     @BeforeEach
@@ -39,29 +46,29 @@ public class StatsFactoryTest {
             "30/09/2019"};
         ArrayList<String> expectedLineChartProperties = new ArrayList<>(Arrays.asList(dates));
         //======calories================================================================================================
-        StatsFactory caloriesStatsFactory = new StatsFactory(eb, "linechart", "calories",
-                new Date("25/09/2019"), new Date("30/09/2019"));
+        StatsFactory caloriesStatsFactory = new StatsFactory(eb, VALID_LINE_CHART, VALID_STATISTIC_CATEGORY_CALORIES,
+                new Date(START_DATE), new Date(END_DATE));
         Statistic actualCaloriesStatistic = caloriesStatsFactory.generateStatistic();
 
         Double[] caloriesArr = new Double[]{0.0, 111.0, 300.0, 261.0, 0.0, 0.0};
         ArrayList<Double> expectedCaloriesValues = new ArrayList<>(Arrays.asList(caloriesArr));
 
-        Statistic expectedCaloriesStatistic = new StatisticBuilder().withCategory("calories").withChart("linechart")
-                .withStartDate(new Date("25/09/2019")).withEndDate(new Date("30/09/2019"))
+        Statistic expectedCaloriesStatistic = new StatisticBuilder().withCategory(VALID_STATISTIC_CATEGORY_CALORIES)
+                .withChart(VALID_LINE_CHART).withStartDate(new Date(START_DATE)).withEndDate(new Date(END_DATE))
                 .withProperties(expectedLineChartProperties).withValues(expectedCaloriesValues).build();
 
         assertEquals(expectedCaloriesStatistic, actualCaloriesStatistic);
 
         //======exercise================================================================================================
-        StatsFactory exerciseStatsFactory = new StatsFactory(eb, "linechart", "exercise",
-                new Date("25/09/2019"), new Date("30/09/2019"));
+        StatsFactory exerciseStatsFactory = new StatsFactory(eb, VALID_LINE_CHART, VALID_STATISTIC_CATEGORY_EXERCISE,
+                new Date(START_DATE), new Date(END_DATE));
         Statistic actualExerciseStatistic = exerciseStatsFactory.generateStatistic();
 
         Double[] valuesArr = new Double[]{0.0, 1.0, 1.0, 2.0, 0.0, 0.0};
         ArrayList<Double> expectedExerciseValues = new ArrayList<>(Arrays.asList(valuesArr));
 
-        Statistic expectedExerciseStatistic = new StatisticBuilder().withCategory("exercise").withChart("linechart")
-                .withStartDate(new Date("25/09/2019")).withEndDate(new Date("30/09/2019"))
+        Statistic expectedExerciseStatistic = new StatisticBuilder().withCategory(VALID_STATISTIC_CATEGORY_EXERCISE)
+                .withChart(VALID_LINE_CHART).withStartDate(new Date(START_DATE)).withEndDate(new Date(END_DATE))
                 .withProperties(expectedLineChartProperties).withValues(expectedExerciseValues).build();
 
         assertEquals(expectedExerciseStatistic, actualExerciseStatistic);
@@ -73,29 +80,29 @@ public class StatsFactoryTest {
         String[] propertiesArr = new String[]{"Cycling (km)", "Running (hours)", "Swimming (laps)"};
         ArrayList<String> expectedProperties = new ArrayList<>(Arrays.asList(propertiesArr));
         //=====calories=================================================================================================
-        StatsFactory caloriesStatsFactory = new StatsFactory(eb, "barchart", "calories",
-                new Date("25/09/2019"), new Date("30/09/2019"));
+        StatsFactory caloriesStatsFactory = new StatsFactory(eb, VALID_BAR_CHART, VALID_STATISTIC_CATEGORY_CALORIES,
+                new Date(START_DATE), new Date(END_DATE));
         Statistic actualCaloriesStatistic = caloriesStatsFactory.generateStatistic();
 
         Double[] caloriesArr = new Double[]{150.0, 222.0, 300.0};
         ArrayList<Double> expectedCalories = new ArrayList<>(Arrays.asList(caloriesArr));
 
-        Statistic expectedCaloriesStatistic = new StatisticBuilder().withCategory("calories").withChart("barchart")
-                .withStartDate(new Date("25/09/2019")).withEndDate(new Date("30/09/2019"))
+        Statistic expectedCaloriesStatistic = new StatisticBuilder().withCategory(VALID_STATISTIC_CATEGORY_CALORIES)
+                .withChart(VALID_BAR_CHART).withStartDate(new Date(START_DATE)).withEndDate(new Date(END_DATE))
                 .withProperties(expectedProperties).withValues(expectedCalories).build();
 
         assertEquals(expectedCaloriesStatistic, actualCaloriesStatistic);
 
         //=====exercise=================================================================================================
-        StatsFactory exerciseStatsFactory = new StatsFactory(eb, "barchart", "exercise",
-                new Date("25/09/2019"), new Date("30/09/2019"));
+        StatsFactory exerciseStatsFactory = new StatsFactory(eb, VALID_BAR_CHART, VALID_STATISTIC_CATEGORY_EXERCISE,
+                new Date(START_DATE), new Date(END_DATE));
         Statistic actualExerciseStatistic = exerciseStatsFactory.generateStatistic();
 
         Double[] valuesArr = new Double[]{5.0, 1.5, 10.0};
         ArrayList<Double> expectedValues = new ArrayList<>(Arrays.asList(valuesArr));
 
-        Statistic expectedExerciseStatistic = new StatisticBuilder().withCategory("exercise").withChart("barchart")
-                .withStartDate(new Date("25/09/2019")).withEndDate(new Date("30/09/2019"))
+        Statistic expectedExerciseStatistic = new StatisticBuilder().withCategory(VALID_STATISTIC_CATEGORY_EXERCISE)
+                .withChart(VALID_BAR_CHART).withStartDate(new Date(START_DATE)).withEndDate(new Date(END_DATE))
                 .withProperties(expectedProperties).withValues(expectedValues).build();
 
         assertEquals(expectedExerciseStatistic, actualExerciseStatistic);
@@ -106,29 +113,29 @@ public class StatsFactoryTest {
         String[] propertiesArr = new String[]{"Cycling (km)", "Running (hours)", "Swimming (laps)"};
         ArrayList<String> expectedProperties = new ArrayList<>(Arrays.asList(propertiesArr));
         //=====calories=================================================================================================
-        StatsFactory caloriesStatsFactory = new StatsFactory(eb, "piechart", "calories",
-                new Date("25/09/2019"), new Date("30/09/2019"));
+        StatsFactory caloriesStatsFactory = new StatsFactory(eb, VALID_PIE_CHART, VALID_STATISTIC_CATEGORY_CALORIES,
+                new Date(START_DATE), new Date(END_DATE));
         Statistic actualCaloriesStatistic = caloriesStatsFactory.generateStatistic();
 
         Double[] caloriesArr = new Double[]{150.0, 222.0, 300.0};
         ArrayList<Double> expectedCalories = new ArrayList<>(Arrays.asList(caloriesArr));
 
-        Statistic expectedCaloriesStatistic = new StatisticBuilder().withCategory("calories").withChart("piechart")
-                .withStartDate(new Date("25/09/2019")).withEndDate(new Date("30/09/2019"))
+        Statistic expectedCaloriesStatistic = new StatisticBuilder().withCategory(VALID_STATISTIC_CATEGORY_CALORIES)
+                .withChart(VALID_PIE_CHART).withStartDate(new Date(START_DATE)).withEndDate(new Date(END_DATE))
                 .withProperties(expectedProperties).withValues(expectedCalories).build();
 
         assertEquals(expectedCaloriesStatistic, actualCaloriesStatistic);
 
         //=====exercise=================================================================================================
-        StatsFactory exerciseStatsFactory = new StatsFactory(eb, "piechart", "exercise",
-                new Date("25/09/2019"), new Date("30/09/2019"));
+        StatsFactory exerciseStatsFactory = new StatsFactory(eb, VALID_PIE_CHART, VALID_STATISTIC_CATEGORY_EXERCISE,
+                new Date(START_DATE), new Date(END_DATE));
         Statistic actualExerciseStatistic = exerciseStatsFactory.generateStatistic();
 
         Double[] valuesArr = new Double[]{1.0, 2.0, 1.0};
         ArrayList<Double> expectedValues = new ArrayList<>(Arrays.asList(valuesArr));
 
-        Statistic expectedExerciseStatistic = new StatisticBuilder().withCategory("exercise").withChart("piechart")
-                .withStartDate(new Date("25/09/2019")).withEndDate(new Date("30/09/2019"))
+        Statistic expectedExerciseStatistic = new StatisticBuilder().withCategory(VALID_STATISTIC_CATEGORY_EXERCISE)
+                .withChart(VALID_PIE_CHART).withStartDate(new Date(START_DATE)).withEndDate(new Date(END_DATE))
                 .withProperties(expectedProperties).withValues(expectedValues).build();
 
         assertEquals(expectedExerciseStatistic, actualExerciseStatistic);
--- src/test/java/seedu/exercise/logic/parser/StatsCommandParserTest.java
+++ src/test/java/seedu/exercise/logic/parser/StatsCommandParserTest.java
@@ -1,12 +1,20 @@
 package seedu.exercise.logic.parser;
 
 import static seedu.exercise.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.exercise.logic.parser.CliSyntax.PREFIX_CATEGORY;
-import static seedu.exercise.logic.parser.CliSyntax.PREFIX_CHART;
-import static seedu.exercise.logic.parser.CliSyntax.PREFIX_END_DATE;
-import static seedu.exercise.logic.parser.CliSyntax.PREFIX_START_DATE;
 import static seedu.exercise.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.exercise.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.exercise.testutil.CommonTestData.END_DATE_DESC;
+import static seedu.exercise.testutil.CommonTestData.INVALID_CHART_DESC;
+import static seedu.exercise.testutil.CommonTestData.INVALID_END_DATE_DESC;
+import static seedu.exercise.testutil.CommonTestData.INVALID_END_DATE_TOO_FAR_APART_DESC;
+import static seedu.exercise.testutil.CommonTestData.INVALID_STATISTIC_CATEGORY_DESC;
+import static seedu.exercise.testutil.CommonTestData.LINE_CHART_DESC;
+import static seedu.exercise.testutil.CommonTestData.START_DATE_DESC;
+import static seedu.exercise.testutil.CommonTestData.STATISTIC_CATEGORY_DESC;
+import static seedu.exercise.testutil.CommonTestData.VALID_END_DATE;
+import static seedu.exercise.testutil.CommonTestData.VALID_LINE_CHART;
+import static seedu.exercise.testutil.CommonTestData.VALID_START_DATE;
+import static seedu.exercise.testutil.CommonTestData.VALID_STATISTIC_CATEGORY_CALORIES;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,42 +24,22 @@ import seedu.exercise.model.property.Date;
 
 public class StatsCommandParserTest {
 
-    public static final String VALID_CHART = "linechart";
-    public static final String VALID_END_DATE = "23/10/2019";
-    public static final String VALID_START_DATE = "17/10/2019";
-    public static final String VALID_STATISTIC_CATEGORY = "calories";
-
-    public static final String CHART_DESC = " " + PREFIX_CHART + VALID_CHART;
-    public static final String END_DATE_DESC = " " + PREFIX_END_DATE + VALID_END_DATE;
-    public static final String START_DATE_DESC = " " + PREFIX_START_DATE + VALID_START_DATE;
-    public static final String STATISTIC_CATEGORY_DESC = " " + PREFIX_CATEGORY + VALID_STATISTIC_CATEGORY;
-
-    public static final String INVALID_CHART = "invalidchart";
-    public static final String INVALID_END_DATE = "01/01/2019"; //invalid because is before start date
-    public static final String INVALID_END_DATE_TOO_FAR_APART = "12/12/2019";
-    public static final String INVALID_STATISTIC_CATEGORY = "invalidcalories";
-
-    public static final String INVALID_CHART_DESC = " " + PREFIX_CHART + INVALID_CHART;
-    public static final String INVALID_STATISTIC_CATEGORY_DESC = " " + PREFIX_CATEGORY + INVALID_STATISTIC_CATEGORY;
-    public static final String INVALID_END_DATE_DESC = " " + PREFIX_END_DATE + INVALID_END_DATE;
-    public static final String INVALID_END_DATE_TOO_FAR_APART_DESC = " " + PREFIX_END_DATE
-            + INVALID_END_DATE_TOO_FAR_APART;
-
     private StatsCommandParser parser = new StatsCommandParser();
 
     @Test
     public void allFieldsPresent_success() {
-        StatsCommand expectedStatsCommand = new StatsCommand(VALID_CHART, VALID_STATISTIC_CATEGORY,
+        StatsCommand expectedStatsCommand = new StatsCommand(VALID_LINE_CHART, VALID_STATISTIC_CATEGORY_CALORIES,
                 new Date(VALID_START_DATE), new Date(VALID_END_DATE));
 
-        assertParseSuccess(parser, CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC + END_DATE_DESC,
-            expectedStatsCommand);
+        assertParseSuccess(parser, LINE_CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC
+            + END_DATE_DESC, expectedStatsCommand);
     }
 
     @Test
     public void parse_optionalFieldMissing_success() {
-        StatsCommand expectedStatsCommand = new StatsCommand(VALID_CHART, VALID_STATISTIC_CATEGORY, null, null);
-        assertParseSuccess(parser, CHART_DESC + STATISTIC_CATEGORY_DESC, expectedStatsCommand);
+        StatsCommand expectedStatsCommand = new StatsCommand(VALID_LINE_CHART, VALID_STATISTIC_CATEGORY_CALORIES,
+                null, null);
+        assertParseSuccess(parser, LINE_CHART_DESC + STATISTIC_CATEGORY_DESC, expectedStatsCommand);
     }
 
     @Test
@@ -59,16 +47,16 @@ public class StatsCommandParserTest {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, StatsCommand.MESSAGE_USAGE);
 
         //missing chart prefix
-        assertParseFailure(parser, VALID_CHART + STATISTIC_CATEGORY_DESC + START_DATE_DESC + END_DATE_DESC,
-                expectedMessage);
+        assertParseFailure(parser, VALID_LINE_CHART + STATISTIC_CATEGORY_DESC + START_DATE_DESC
+                + END_DATE_DESC, expectedMessage);
 
         //missing category prefix
-        assertParseFailure(parser, CHART_DESC + VALID_STATISTIC_CATEGORY + START_DATE_DESC + END_DATE_DESC,
-                expectedMessage);
+        assertParseFailure(parser, LINE_CHART_DESC + VALID_STATISTIC_CATEGORY_CALORIES + START_DATE_DESC
+                + END_DATE_DESC, expectedMessage);
 
         //all prefix missing
-        assertParseFailure(parser, VALID_CHART + VALID_STATISTIC_CATEGORY + VALID_START_DATE + VALID_END_DATE,
-                expectedMessage);
+        assertParseFailure(parser, VALID_LINE_CHART + VALID_STATISTIC_CATEGORY_CALORIES + VALID_START_DATE
+                + VALID_END_DATE, expectedMessage);
     }
 
     @Test
@@ -78,23 +66,23 @@ public class StatsCommandParserTest {
                 + START_DATE_DESC + END_DATE_DESC, Statistic.MESSAGE_INVALID_CHART_TYPE);
 
         //invalid category
-        assertParseFailure(parser, CHART_DESC + INVALID_STATISTIC_CATEGORY_DESC
+        assertParseFailure(parser, LINE_CHART_DESC + INVALID_STATISTIC_CATEGORY_DESC
                 + START_DATE_DESC + END_DATE_DESC, Statistic.MESSAGE_INVALID_CATEGORY);
 
         //start date present, end date not present
-        assertParseFailure(parser, CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC,
+        assertParseFailure(parser, LINE_CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC,
                 StatsCommandParser.MESSAGE_INVALID_COMMAND);
 
         //end date present, start date not present
-        assertParseFailure(parser, CHART_DESC + STATISTIC_CATEGORY_DESC + END_DATE_DESC,
+        assertParseFailure(parser, LINE_CHART_DESC + STATISTIC_CATEGORY_DESC + END_DATE_DESC,
                 StatsCommandParser.MESSAGE_INVALID_COMMAND);
 
         //end date is before start date
-        assertParseFailure(parser, CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC + INVALID_END_DATE_DESC,
-                Date.MESSAGE_INVALID_END_DATE);
+        assertParseFailure(parser, LINE_CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC
+                + INVALID_END_DATE_DESC, Date.MESSAGE_INVALID_END_DATE);
 
         //start date and end date too far apart
-        assertParseFailure(parser, CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC
+        assertParseFailure(parser, LINE_CHART_DESC + STATISTIC_CATEGORY_DESC + START_DATE_DESC
                 + INVALID_END_DATE_TOO_FAR_APART_DESC, StatsCommandParser.MESSAGE_INVALID_DATE_RANGE);
     }
 }
